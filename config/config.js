15b9d74fd6ebe385e0cf6d53b8cab4331d7fc16e
stove <stove654@gmail.com>
2017-06-07 14:35:32 +0700

    update config db

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: 'ilovejavascript',
-    database: 'mongodb://localhost:27017/stove',
+    database: 'mongodb://localhost:27017/vicare',
     userRoles: ['guest', 'user', 'admin']
 };
